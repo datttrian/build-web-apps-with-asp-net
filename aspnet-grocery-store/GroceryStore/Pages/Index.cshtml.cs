2bd95eca68af9dda73da2c4412a31fe392ac532f
Dat T. Tran <datttrian@gmail.com>
2023-06-09 09:26:10 -0400

    Add Grocer.ly

---
+++ aspnet-grocery-store/GroceryStore/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+using GroceryStore.Models;
+
+namespace GroceryStore.Pages
+{
+  public class IndexModel : PageModel
+  {
+
+    [BindProperty]
+    public int Rating { get; set; }
+    [BindProperty]
+    public string Feedback { get; set; }
+
+    public List<GroceryItem> Foods = Inventory.ToList();
+
+    public void OnGet()
+    {
+
+    }
+
+    public async Task OnPostAsync()
+    {
+      using (StreamWriter writer = new StreamWriter("feedback.txt", append: true))
+      {
+        await writer.WriteLineAsync($"{DateTime.Now} - Rating: {Rating}");
+        await writer.WriteLineAsync(Feedback);
+      }
+    }
+  }
+}
---
+++ aspnet-grocery-store/README.md
@@ -0,0 +1,5 @@
+# Grocer.ly
+
+[Learn ASP.NET Grocer.ly](https://www.youtube.com/watch?v=EFTQ_tzdIxU)
+
+**Reference:** https://www.codecademy.com/paths/build-web-apps-with-asp-net/tracks/asp-net-i/modules/asp-net-page-models/projects/aspnet-grocery-store
